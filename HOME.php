e0d7b5b27e3155f2af31e9af73bdc28c94960008
rhmtlns <90814493+rhmtlns@users.noreply.github.com>
2021-12-21 02:20:53 +0700

    Add files via upload

---
+++ HOME.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charsheet="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Rohmatul Anisa</title>
+        <link rel="stylesheet" href="UTS.css">
+    </head>
+
+    <body>
+        <div class='header'>
+            <div class="header-logo">
+                <img class="rounded-image" src="R.A.png">
+            </div>
+            <div class='header-list'>
+                <ul>
+                    <li>HOME</li>
+                    <li><a href="TOKO.php" >ABOUT US</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="main">
+            <div class="contents">
+                <h1><b>SELAMAT DATANG DI TOKO KAMI</b></h1>
+                <h2>Selamat berbelanja dengan senang hati.</h2>
+                <img src="gooo.png" width="260px" height="260px">
+            </div>
+        </div>      
+    </body>
+</html>
